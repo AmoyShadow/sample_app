7337888888ae990cd7f5aee1700ea99da3a72d68
Minxi Li <5330555@qq.com>
2014-04-12 20:54:12 +0800

    3.5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,6 +9,8 @@ require 'spec_helper'
 #     end
 #   end
 describe "Static pages" do
+
+  let(:base_title) {"Ruby on Rails Tutorial Sample App"}
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -17,7 +19,7 @@ describe "Static pages" do
 
     it "should have the right title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Home')
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -29,7 +31,7 @@ describe "Static pages" do
 
     it "should have the tilte 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
@@ -41,7 +43,14 @@ describe "Static pages" do
 
     it "should have the title 'About'" do
       visit '/static_pages/about'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
+      expect(page).to have_title("#{base_title} | About")
+    end
+  end
+
+  describe "contact" do
+    it "should have the title 'contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
